d49f0b95d7d835cad1357a7be3ca5dced1e4e0e6
ev0xP <evoprimitive@gmail.com>
2026-09-08 16:34:58 +0700

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="openvoice",
     version="2.0.0",
-    packages=find_packages(include=["openvoice", "openvoice.*"]),
+    packages=find_packages(where="."),
+    package_dir={"": "."},
     include_package_data=True,
     install_requires=[
         "torch>=2.0.0",
